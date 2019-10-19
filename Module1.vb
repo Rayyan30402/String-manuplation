9a392fe48e0a61272a79fcf87418c8eec5b7e7f7
Rayyan30402 <55427085+Rayyan30402@users.noreply.github.com>
2019-10-19 22:47:52 +0500

    char replacing

--- Module1.vb
+++ Module1.vb
@@ -3,41 +3,40 @@
     Sub Main()
         Dim String1 As String
         Dim String2 As String
-        Dim String3 As String
-        Dim nextchar1 As String
-        Dim nextchar2 As String
-        Dim counter1 As Integer
-        Dim counter2 As Integer
-        Dim haschanged As Boolean
+        Dim Char1 As Char
+        Dim Char2 As Char
+        Dim nextchar As Char
+        Dim Counter As Integer
 
         String1 = ""
         String2 = ""
-        String3 = ""
-        nextchar1 = ""
-        nextchar2 = ""
-        counter1 = 0
-        counter2 = 0
-        haschanged = True / False
+        Char1 = ""
+        Char2 = ""
+        nextchar = ""
+        Counter = 0
 
-        For counter1 = 1 To Len(String1)
-            nextchar1 = Mid(String1, counter1, 1)
-            String3 = ""
-            haschanged = False
-               counter2 = 1 To Len(String2)
-            nextchar2 = Mid(String2, counter2, 1)
-            If nextchar2 = nextchar1 Then
+        Console.Write("Enter String1 : ")
+        String1 = Console.ReadLine
 
-                haschanged = False
-            Else
-                haschanged = True
-                    console.write("String3="'String3 & nextxhar1')
+        Console.Write("Enter character to remove: ")
+        Char1 = Console.ReadLine
+
+        Console.Write("Enter character to replce: ")
+        Char2 = Console.ReadLine
+
+        For Counter = 1 To Len(String1)
+            nextchar = Mid(String1, Counter, 1)
+            If nextchar = Char1 Then
+                nextchar = Char2
             End If
-
-
-
+            String2 = String2 & nextchar
         Next
-        String2 = String3
+
+        Console.WriteLine("Final string = " & String2)
         Console.ReadKey()
+
+
+
     End Sub
 
 End Module
