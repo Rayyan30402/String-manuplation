02210c847a5c06ced0373b90fa231dc9acb425b8
Rayyan30402 <55427085+Rayyan30402@users.noreply.github.com>
2019-10-28 19:57:32 +0500

    Add files via upload

---
+++ test1.vb
@@ -0,0 +1,33 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim validBinary As Boolean
+        Dim Binarynumbers As Integer
+        Dim Counter1 As Integer
+        Dim nextnum As Integer
+
+        validBinary = True / False
+        Binarynumbers = 0
+        Counter1 = 0
+        nextnum = 0
+
+        Console.WriteLine("enter Binarynumbers")
+        Binarynumbers = Console.ReadLine
+
+        For Counter1 = 1 To 8
+              Mid(Binarynumbers, Counter1, 1)
+            If Binarynumbers = 0 And 1 Then
+                Console.WriteLine("it is a valid Binary string")
+            End If
+            validBinary = True
+         ElseIf 
+            nextnum = Binarynumbers
+
+            validBinary = True
+            Else
+            validBinary = False
+
+        Next
+
+    End Sub
+End Module
